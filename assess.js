9a73bf8e91776efe61b178527e3a60f671751b0e
Tu Binh Minh <minhkkjc@gmail.com>
2015-08-03 11:02:43 +0700

    Fix videoassessment

--- assess.js
+++ assess.js
@@ -4,11 +4,14 @@ jQuery(function($) {
     $(window).scroll(function() {
         var $video = $('.assess-form-videos .video');
         var $video_top = $video.parent().offset().top;
+        var $video_height = $video.height();
+        var $form = $('.path-mod-videoassessment .gradingform');
+        var $scroll_form = $form.offset().top + $form.height();
 
-        if ($(this).scrollTop() >= ($video_top - 62)) {
+        if ($(this).scrollTop() >= ($video_top - 62) && $(this).scrollTop() < ($scroll_form - $video_height - 62)) {
             var $padding = $(this).scrollTop() - $video_top + 62;
             $video.css({'padding-top' : $padding});
-        } else {
+        } else if ($(this).scrollTop() < ($video_top - 62)) {
             $video.css({'padding-top' : 0});
         }
     });
